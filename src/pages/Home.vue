0e3dbd98af647c3757683f64c434e08bad1d0d12
Levent <->
2019-02-01 23:10:14 +0100

    Add ajax test function

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,10 +2,19 @@
   <div></div>
 </template>
 <script>
+
 export default {
-  
+  mounted() {
+    fetch("http://api.levent-deniz.de/ajax/")
+            .then(response => response.json())
+            .then((data) => {
+              console.log(data);
+            })
+  }
 }
+
 </script>
+
 <style scoped>
 
 </style>
