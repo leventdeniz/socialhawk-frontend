2f37dfe86f09176e5a8883ea153ee811c965755b
Unknown <zaj.dev.learning@gmail.com>
2019-02-01 21:06:35 +0100

    implement vue router
    - create /
    - create /login
    - create /register

--- src/main.js
+++ src/main.js
@@ -1,14 +1,32 @@
 import 'onsenui/css/onsenui.css';
 import 'onsenui/css/onsen-css-components.css';
 
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import VueOnsen from 'vue-onsenui';
+import VueRouter from 'vue-router';
 
-Vue.config.productionTip = false
+import Home from './pages/Home.vue';
+import Login from './pages/Login.vue';
+import Register from './pages/Register.vue';
+
+Vue.config.productionTip = false;
+
 Vue.use(VueOnsen);
+Vue.use(VueRouter);
 
+const routes = [
+  { path: '/', component: Home}, 
+  { path: '/login', component: Login},
+  { path: '/register', component: Register},
+];
+
+const router = new VueRouter({
+  routes, 
+  mode: 'history'
+});
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  router,
+}).$mount('#app');
--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,17 +1,14 @@
 <template>
-  <div id="app">
-    <router-view>
-
-    </router-view>
-  </div>
+  <div></div>
 </template>
-
 <script>
 export default {
   
 }
 </script>
-
-<style>
+<style scoped>
 
 </style>
+
+
+
