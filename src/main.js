c8187d350c2eee6b321860e8ddbaad4e59fae426
Unknown <zaj.dev.learning@gmail.com>
2019-02-01 11:41:34 +0100

    add onsen ui

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
+
 import Vue from 'vue'
 import App from './App.vue'
+import VueOnsen from 'vue-onsenui';
 
 Vue.config.productionTip = false
+Vue.use(VueOnsen);
+
 
 new Vue({
   render: h => h(App),
