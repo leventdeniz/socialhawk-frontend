082f25e9dbdc158ccacd1e545f5033dc32c786d8
Unknown <zaj.dev.learning@gmail.com>
2019-02-02 00:34:31 +0100

    add landingpage & add login/register routes

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,22 +1,41 @@
 <template>
-  <div></div>
+  <section class="flex-center">
+      <v-ons-button modifier="button" @click="login">Sign In</v-ons-button>
+      <v-ons-button modifier="button" @click="register">Sign Out</v-ons-button>
+  </section>
 </template>
 <script>
 
 export default {
-  mounted() {
+  /* mounted() {
     fetch("http://api.levent-deniz.de/ajax")
             .then(response => response.json())
             .then((data) => {
               console.log(data);
-            })
+    })
+  },  */
+  methods: {
+    login(){
+      this.$router.push('/login');
+    },
+    register(){
+      this.$router.push('/register');
+    }
   }
 }
-
 </script>
 
 <style scoped>
+.flex-center {
+  display: flex;
+  justify-content: center;  
+}
 
+ons-button[modifier="button"]{
+  width: 50%;
+  text-align: center;
+  background: #b2811e;
+}
 </style>
 
 
