40724916124e0c13ba0b9770d82a056dce48300b
Levent <->
2019-02-04 10:18:53 +0100

    Make style changes to Register

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,52 +1,39 @@
-<template>
-
+<template id="register">
     <v-ons-page>
-        <div class="center">
-            <h3>Welcome to [APP NAME]!</h3>
-            <p>the number one platform for influencer marketing worldwide!</p>
+
+        <section class="register-header">
+            <h3>Welcome to [APPNAME]!</h3>
+            <span>the number one platform for influencer marketing!</span>
+        </section>
+
+        <div class="register-area">
+            <div class="center register-element">
+                <v-ons-input placeholder="email" v-model="email"></v-ons-input>
+            </div>
+            <div class="center register-element">
+                <v-ons-input placeholder="password" type="password" modifier="underbar"
+                             v-model="password"></v-ons-input>
+            </div>
+            <div class="center register-element">
+                <v-ons-input placeholder="password" type="password" modifier="underbar"
+                             v-model="passwordconfirm"></v-ons-input>
+            </div>
+            <div class="center register-element">
+                <v-ons-input placeholder="username" modifier="underbar" v-model="username"></v-ons-input>
+            </div>
+            <div class="center register-element register-checkbox">
+                <v-ons-checkbox v-model="terms"></v-ons-checkbox>
+                by confirming this checkbox, you agree to our <a href="http://levent-deniz.de">terms of service</a>.
+            </div>
+
+            <div class="center register-element register-button">
+                <v-ons-button @click="register">Register</v-ons-button>
+            </div>
         </div>
 
-        <v-ons-list class="register">
-            <v-ons-list-item>
-                <div class="center">
-                    <v-ons-input placeholder="email" v-model="email"></v-ons-input>
-                </div>
-            </v-ons-list-item>
-            <v-ons-list-item>
-                <div class="center">
-                    <v-ons-input placeholder="password" type="password" modifier="underbar"
-                                 v-model="password"></v-ons-input>
-                </div>
-            </v-ons-list-item>
-            <v-ons-list-item>
-                <div class="center">
-                    <v-ons-input placeholder="password" type="password" modifier="underbar"
-                                 v-model="passwordconfirm"></v-ons-input>
-                </div>
-            </v-ons-list-item>
-            <v-ons-list-item>
-                <div class="center">
-                    <v-ons-input placeholder="username" modifier="underbar" v-model="username"></v-ons-input>
-                </div>
-            </v-ons-list-item>
-            <v-ons-list-item>
-                <div class="center">
-                    <v-ons-checkbox v-model="terms"></v-ons-checkbox>
-                    by confirming this checkbox, you agree to our <a href="http://levent-deniz.de">terms of service</a>.
-                </div>
-            </v-ons-list-item>
-        </v-ons-list>
-
-        <p style="text-align: center">
-            <v-ons-button @click="register">Sign Up</v-ons-button>
-        </p>
-
-        <section>
-            <div class="login-note">
-                already have an account? sign in
-                <router-link to="/login">here</router-link>
-                !
-            </div>
+        <section  class="center btn-login">
+                already have an account? <br>
+                <router-link to="/login">Log in!</router-link>
         </section>
 
     </v-ons-page>
@@ -79,6 +66,43 @@
     }
 </script>
 <style scoped>
+    .register-header {
+        width: 80%;
+        margin: 10vh auto 0;
+    }
+
+    .register-header h3 {
+        margin-bottom: 0;
+    }
+
+    .register-header span {
+        font-size: 0.85em;
+    }
+
+    .register-area {
+        margin-top: 10vh;
+    }
+
+    .register-element {
+        margin-top: 30px;
+    }
+
+    .register-button {
+        margin-top: 5vh;
+    }
+
+    .register-checkbox {
+        max-width: 80%;
+        margin: 3vh auto 0;
+    }
+    .btn-login {
+        position: absolute;
+        bottom: 2%;
+        left: 40%;
+        transform: translate(-35%);
+    }
+
+
 </style>
 
 
