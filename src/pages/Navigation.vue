3c82cf11b98bb14467c210eb2f8cb50ac91435a4
Levent <->
2019-02-09 15:05:40 +0100

    File indentation

--- src/pages/Navigation.vue
+++ src/pages/Navigation.vue
@@ -1,71 +1,78 @@
 <template>
-  <v-ons-page>
-    <v-ons-toolbar>
-      <div class="center">{{ title }}</div>
-      <div class="right">
-        <v-ons-toolbar-button>
-          <v-ons-icon :icon="icon"></v-ons-icon>
-        </v-ons-toolbar-button>
-      </div>
-    </v-ons-toolbar>
-    <v-ons-tabbar swipeable position="bottom" :tabs="tabs" :visible="true" :index.sync="activeIndex"></v-ons-tabbar>
-  </v-ons-page>
+    <v-ons-page>
+        <v-ons-toolbar>
+            <div class="center">{{ title }}</div>
+            <div class="right">
+                <v-ons-toolbar-button>
+                    <v-ons-icon :icon="icon"></v-ons-icon>
+                </v-ons-toolbar-button>
+            </div>
+        </v-ons-toolbar>
+        <v-ons-tabbar
+                swipeable
+                position="bottom"
+                :tabs="tabs"
+                :visible="true"
+                :index.sync="activeIndex">
+        </v-ons-tabbar>
+    </v-ons-page>
 </template>
 <script>
-import Dashboard from "./Dashboard.vue";
-import Profile from "./Profile.vue";
-import Campaigns from "./Campaigns.vue";
+    import Dashboard from "./Dashboard.vue";
+    import Profile from "./Profile.vue";
+    import Campaigns from "./Campaigns.vue";
 
-export default {    
-  components: {
-    Dashboard,
-    Profile,
-    Campaigns
-  },
-  data: function() {
-    return {
-      activeIndex: 1,
-      tabs: [
-        {
-          icon: "ion-person",
-          title: "Profile",
-          page: Profile,
-          key: "profile"
+    export default {
+        components: {
+            Dashboard,
+            Profile,
+            Campaigns
         },
-        {
-          icon: "ion-home",
-          title: "Dashboard",
-          page: Dashboard,
-          key: "dashboard",
-          item: "fa-envelope"
+        data: function () {
+            return {
+                activeIndex: 1,
+                tabs: [
+                    {
+                        icon: "ion-person",
+                        title: "Profile",
+                        page: Profile,
+                        key: "profile"
+                    },
+                    {
+                        icon: "ion-home",
+                        title: "Dashboard",
+                        page: Dashboard,
+                        key: "dashboard",
+                        item: "fa-envelope"
+                    },
+                    {
+                        icon: "ion-filing",
+                        title: "Campaigns",
+                        page: Campaigns,
+                        key: "campaigns",
+                    }
+                ]
+            };
         },
-        {
-          icon: "ion-filing",
-          title: "Campaigns",
-          page: Campaigns,
-          key: "campaigns",
-        }
-      ]
+        computed: {
+            title() {
+                return this.tabs[this.activeIndex].title;
+            },
+            icon() {
+                return this.tabs[this.activeIndex].item;
+            }
+        },
     };
-  },
-  computed: {
-    title() {
-      return this.tabs[this.activeIndex].title;
-    },
-    icon(){
-        return this.tabs[this.activeIndex].item;
-    }
-  }, 
-};
 </script>
 <style scoped>
     .center {
         text-align: initial;
     }
- .fa-envelope{
-     color: grey;
 
- }
+    .fa-envelope {
+        color: grey;
+
+    }
 </style>
 
 
